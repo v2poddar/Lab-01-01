70ae24a49a433a945087fdc0f48d777810e7e37e
v2poddar <99185695+v2poddar@users.noreply.github.com>
2022-02-07 18:24:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Ahmad</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing">
+
+		<!-- Banner -->
+			<section id="banner">
+				<h2>AWS Developer LAB</h2>
+				<! TO DO >
+<p class="ui header center"><font color="white">Server IP Address <?= $_SERVER['SERVER_ADDR'] ?></
+				<ul class="actions">
+					<li><a href="#" class="button special big">Get Started</a></li>
+				</ul>
+			</section>
+
+			
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook"></a></li>
+						<li><a href="#" class="icon fa-twitter"></a></li>
+						<li><a href="#" class="icon fa-instagram"></a></li>
+					</ul>
+					
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
